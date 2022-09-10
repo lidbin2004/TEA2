94dd3dcfb2cab1be05fbe5278a398a3eb73b5815
Lidbin Oswaldo Pacheco Reyes (24120) <lidbin.pacheco@est.zamorano.edu>
2022-09-09 19:46:42 -0600

    imprimir nombre

---
+++ lab02-constante y variables.py
@@ -0,0 +1,22 @@
+# Teendencias e Innovacion en Tecnologia Agricola (TEA)
+message = "Este es el laboratorio 2 de TEA"
+temperature = 40.5
+pi = 3.14591654
+
+#display message 
+print(message)
+
+#displaythe data type of message:
+print(type(message))
+
+#display temperature:
+print(temperature )
+
+# modificar la temperature 
+temperature = 45.1
+
+#display temperature
+print(temperature)
+
+#display the data type of temperature:
+print(type(temperature))
